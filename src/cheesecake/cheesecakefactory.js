e7071a8a9c65ca0a4116492f9e1d4fe74cb0abbe
NathanR <dnr500@gmail.com>
2015-11-18 21:20:03 +0000

    refactor of the factory

--- src/cheesecake/cheesecakefactory.js
+++ src/cheesecake/cheesecakefactory.js
@@ -13,25 +13,13 @@ require.def('cheesecake/cheesecakefactory',
         'use strict';
 
         return {
-
             _isNoneEmptyArray: function (element) {
                 return element instanceof Array && element.length > 0;
             },
 
             _processChildren: function (parent, childrenData) {
                 for (var i = 0, len = childrenData.length; i < len; i++) {
-                    var childWidgetData = childrenData[i];
-                    var recipe = CheeseCakeRecipes.getRecipe(childWidgetData.recipeName);
-
-                    var childWidget = recipe(childWidgetData, parent);
-
-                    if (this._isNoneEmptyArray(childWidgetData.children)) {
-                        this._processChildren(childWidget, childWidgetData.children);
-                    }
-                    if (this._isNoneEmptyArray(childWidgetData.actions)) {
-                        CheeseCakeActions.bindActionsToWidget(childWidget,
-                            childWidgetData.actions);
-                    }
+                    var childWidget = this._processItem(childrenData[i], parent);
 
                     if(parent){
                         cheeseCakeMappings.appendChild(parent, childWidget);
@@ -41,26 +29,36 @@ require.def('cheesecake/cheesecakefactory',
                 }
             },
 
+            _processItem: function(childData, parent) {
+                var childWidget = this._createFromRecipe(childData, parent);
+                this._createChildren(childData, childWidget);
+                this._createActions(childData, childWidget);
+                return childWidget;
+            },
+
+            _createChildren: function(childData, childWidget) {
+                if (this._isNoneEmptyArray(childData.children)) {
+                    this._processChildren(childWidget, childData.children);
+                }
+            },
+
+            _createActions: function(childData, childWidget) {
+                if (this._isNoneEmptyArray(childData.actions)) {
+                    CheeseCakeActions.bindActionsToWidget(childWidget, childData.actions);
+                }
+            },
+
+            _createFromRecipe: function(childWidgetData, parent) {
+                var recipe = CheeseCakeRecipes.getRecipe(childWidgetData.recipeName);
+                return recipe(childWidgetData, parent);
+            },
+
             mappings : function() {
                 return cheeseCakeMappings;
             },
 
             createCheeseCake: function (data) {
-                var cheesecake = data.cheesecake;
-
-                var recipe = CheeseCakeRecipes.getRecipe(cheesecake.recipeName);
-                var cheeseCakeContainer = recipe(cheesecake);
-
-                if (this._isNoneEmptyArray(cheesecake.actions)) {
-                    CheeseCakeActions.bindActionsToWidget(cheeseCakeContainer,
-                        cheesecake.actions);
-                }
-
-                if(cheesecake.children) {
-                    this._processChildren(cheeseCakeContainer, cheesecake.children);
-                }
-
-                return cheeseCakeContainer;
+                return this._processItem(data.cheesecake);
             },
 
             addRecipe: function (uniqueId, recipe) {
@@ -78,7 +76,6 @@ require.def('cheesecake/cheesecakefactory',
             unbindCheeseCake: function (cheesecakeContainer) {
                 CheeseCakeActions.unbindCheeseCake(cheesecakeContainer);
             }
-
         };
     }
 );
